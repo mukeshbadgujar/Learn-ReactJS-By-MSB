27e7a8577522a7b901505ba724a1ebe80834830c
Mukesh Badgujar <32473209+mukeshbadgujar@users.noreply.github.com>
2020-08-04 13:27:06 +0530

    with normal submit

--- netflix_top/src/InputName.jsx
+++ netflix_top/src/InputName.jsx
@@ -3,20 +3,30 @@ import React, { useState } from "react";
 const InputName = () => {
   const [name, setName] = useState("");
   const [fullName, setFullName] = useState();
+  const [lname, setLName] = useState("");
+  const [fullLName, setFullLName] = useState();
 
   const InputEvent = (e) => {
     setName(e.target.value);
   };
+  const InputEvent2 = (e) => {
+    setLName(e.target.value);
+  };
 
   const onSubmit = () => {
     setFullName(name);
+    setFullLName(lname);
   };
 
   return (
     <React.Fragment>
-      <h1>Name: {fullName} </h1>
+      <h1>
+        Name: {fullName} {fullLName}
+      </h1>
       <h1>Name Update: {name} </h1>
+      <h1>Name L Update: {lname} </h1>
       <input type="text" onChange={InputEvent} value={name} />
+      <input type="text" onChange={InputEvent2} value={lname} />
       <button onClick={onSubmit}>Click</button>
     </React.Fragment>
   );
